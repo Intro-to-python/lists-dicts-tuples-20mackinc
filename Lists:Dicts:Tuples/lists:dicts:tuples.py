23a01f4dd6ea25e96cf53242007631279b11d4c8
Charlotte Mackin <cmackin@cshnyc.org>
2018-09-20 11:35:04 -0400

    notes 9/20/18

--- Lists:Dicts:Tuples/lists:dicts:tuples.py
+++ Lists:Dicts:Tuples/lists:dicts:tuples.py
@@ -2,13 +2,24 @@
 
 #create a list and show how we can find certain items in list
 #add item to list
+dogList = ['golden retriever', 'cavalier king charles', 'chihuahua', 'golden doodle', 'cockapoo']
+print("this is an example of a list:", dogList)
 
 # tuples cannot be changed once created!
 # helpful because sometimes you want to create
 # something that you know will never change
 fibs = (0,1,1,2,3)
+print("this is an example of a tuble:", fibs)
 
 #dictionaries aka dicts aka map
 #key:value pairing
 
-favoriteSports = {}
+favoriteSports = {'Ralph Williams' : 'Football',
+                    'Michael Tippitt' : 'Basketball',
+                    'Edward Elgar' : 'Baseball',
+                    'Rebecca Clark' : 'Netball',
+                    'Ethyl Smith' : 'Badminton',
+                    'Frank Bridge' : 'Rugby'}
+print(favoriteSports['Rebecca Clark'])
+del favoriteSports['Ethyl Smith']
+print(favoriteSports)
